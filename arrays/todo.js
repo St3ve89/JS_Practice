9420b715da61f383d0126fc8deeda81beb821c2e
Istvan Acs <istvan.acs89@gmail.com>
2018-08-21 23:38:59 +0100

    Manipulating arrays with methods

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,9 @@
 const todos = ['Practice code', 'Shop', 'Go to work', 'Go to gym', 'Eat']
 
+todos.splice(2,1)
+todos.push('Buy coffe')
+console.log(todos.shift())
+
+
 console.log(`You have ${todos.length} todos`)
-console.log(`Todo: ${todos[0]} and ${todos[todos.length - 2]}`)
+console.log(todos)
