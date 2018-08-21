4e9e91aff28a81218691b9c4a2858218357c8d74
Istvan Acs <istvan.acs89@gmail.com>
2018-08-21 23:20:33 +0100

    Array basics

---
+++ arrays/todo.js
@@ -0,0 +1,4 @@
+const todos = ['Practice code', 'Shop', 'Go to work', 'Go to gym', 'Eat']
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[0]} and ${todos[todos.length - 2]}`)
