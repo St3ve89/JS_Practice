ea689117dbed5768edc397bb20cfdd66fdd960b7
Istvan Acs <istvan.acs89@gmail.com>
2018-08-26 13:10:57 +0100

    DOM manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,13 @@
 console.log('this is from a different file')
+
+// let p = document.querySelector('p')
+// p.remove()
+
+
+let ps = document.querySelectorAll('p')
+
+ps.forEach(function(p){
+    p.textContent = '******'
+    // console.log(p.textContent)
+    // p.remove()
+})
