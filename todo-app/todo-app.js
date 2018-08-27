611157c75c716d68b36b0571eb3a82e1a382ed62
Istvan Acs <istvan.acs89@gmail.com>
2018-08-27 17:17:27 +0100

    Checkboxes

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -16,12 +16,16 @@ const todos = [{
 }]
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    hideCompleted: false
 }
 
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
     })
 
     const getThingsToDo = filteredTodos.filter(function(todo){
@@ -55,4 +59,9 @@ document.querySelector('#todo-form').addEventListener('submit', function (e) {
     })
     renderTodos(todos, filters)
     e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
 })
