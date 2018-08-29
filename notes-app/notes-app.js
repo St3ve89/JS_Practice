cccdca3d5147881496dcbb241f23426438e25665
Istvan Acs <istvan.acs89@gmail.com>
2018-08-29 22:23:21 +0100

    Using CRUD operations for a local storage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,19 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Istvan',
+//     age: 27
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
