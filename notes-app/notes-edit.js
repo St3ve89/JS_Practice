f13134cc85172fa717016b73fcf064670d44b2aa
Istvan Acs <istvan.acs89@gmail.com>
2018-09-04 23:27:07 +0100

    Change title and body el on the note page

---
+++ notes-app/notes-edit.js
@@ -0,0 +1,31 @@
+const titleEl = document.querySelector('#note-title')
+const bodyEl = document.querySelector('#note-body')
+const removeEl = document.querySelector('#remove-note')
+const noteId = location.hash.substring(1)
+const notes = getSavedNotes()
+const note = notes.find(function (note) {
+    return note.id === noteId
+})
+
+if (note === undefined) {
+    location.assign('/index.html')
+}
+
+titleEl.value = note.title
+bodyEl.value = note.body
+
+titleEl.addEventListener('input', function (e) {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyEl.addEventListener('input', function (e) {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeEl.addEventListener('click', function (e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
