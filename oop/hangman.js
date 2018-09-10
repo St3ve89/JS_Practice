5c388031f322deeaa32bae3b11966237691d2770
Istvan Acs <istvan.acs89@gmail.com>
2018-09-10 22:10:55 +0100

    Setting up the Prototype object

--- oop/hangman.js
+++ oop/hangman.js
@@ -8,4 +8,4 @@ const game1 = new Hangman('cat', 3)
 console.log(game1)
 
 const game2 = new Hangman('London', 5)
-console.log(game2)
+console.log(game2);
--- oop/person.js
+++ oop/person.js
@@ -1,13 +1,31 @@
-const Person = function (firstName, lastName, age) {
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
 }
 
-const me = new Person ('Istvan', 'Acs', 29)
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
 
-console.log(me)
 
-const personTwo = new Person('Agnes', 'Brettl', 27)
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
 
-console.log(personTwo)
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const me = new Person ('Istvan', 'Acs', 29, ['coding', 'gaming'])
+me.setName('Jack Sparrow')
+console.log(me.getBio())
+
+const personTwo = new Person('Agnes', 'Brettl', 27, )
+
+console.log(personTwo.getBio())
