221577983bca12660e4ff3dfcfc392025d9dda79
Istvan Acs <istvan.acs89@gmail.com>
2018-09-15 17:13:22 +0100

    The class syntax

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,14 +1,74 @@
-const Hangman = function(word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    // setup new "status" property with initial value of "playing"
-    this.status = 'playing'
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
+        // setup new "status" property with initial value of "playing"
+        this.status = 'playing'
+    }
+// create method for recalculating status to playing, finished, or failed
+    calculateStatus() {
+        const finished = this.word.every( letter =>  this.guessedLetters.includes(letter))
+
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
+        } else {
+            this.status = 'playing'
+        }
+    }
+// setup a new method to get back a status message
+    getStatusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
+        }
+    }
+
+    getPuzzle() {
+        let puzzle = ''
+
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === '') {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
+
+        return puzzle
+    }
+// create a method for making a guess
+    makeGuess(guess) {
+        this.guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+        // disable new guesses unless 'playing'
+        if (this.status !== 'playing') {
+            return
+        }
+
+        // should add unique guesses to list of guesses
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+        // should decrement the guesses left if a unique guess isn't a match
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+
+        this.calculateStatus()
+    }
 }
 
+
 // create method for recalculating status to playing, finished, or failed
-Hangman.prototype.calculateStatus = function () {
-    const finished = this.word.every( letter =>  this.guessedLetters.includes(letter))
+// Hangman.prototype.calculateStatus = function () {
+//     const finished = this.word.every( letter =>  this.guessedLetters.includes(letter))
     
     
     // const lettersUngessed = this.word.filter(letter => {
@@ -28,58 +88,58 @@ Hangman.prototype.calculateStatus = function () {
     //     }
     // })
 
-    if (this.remainingGuesses === 0) {
-        this.status = 'failed'
-    } else if (finished) {
-        this.status = 'finished'
-    } else {
-        this.status = 'playing'
-    }
-}
+//     if (this.remainingGuesses === 0) {
+//         this.status = 'failed'
+//     } else if (finished) {
+//         this.status = 'finished'
+//     } else {
+//         this.status = 'playing'
+//     }
+// }
 
 // setup a new method to get back a status message
-Hangman.prototype.getStatusMessage = function () {
-    if (this.status === 'playing') {
-        return `Guesses left: ${this.remainingGuesses}`
-    } else if (this.status === 'failed') {
-        return `Nice try! The word was "${this.word.join('')}".`
-    } else {
-        return 'Great work! You guessed the word.'
-    }
-}
+// Hangman.prototype.getStatusMessage = function () {
+//     if (this.status === 'playing') {
+//         return `Guesses left: ${this.remainingGuesses}`
+//     } else if (this.status === 'failed') {
+//         return `Nice try! The word was "${this.word.join('')}".`
+//     } else {
+//         return 'Great work! You guessed the word.'
+//     }
+// }
 
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
+// Hangman.prototype.getPuzzle = function () {
+//     let puzzle = ''
 
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === '') {
-            puzzle += letter
-        } else {
-            puzzle += '*'
-        }
-    })
+//     this.word.forEach((letter) => {
+//         if (this.guessedLetters.includes(letter) || letter === '') {
+//             puzzle += letter
+//         } else {
+//             puzzle += '*'
+//         }
+//     })
 
-    return puzzle
-}
+//     return puzzle
+// }
 
 // create a method for making a guess
-Hangman.prototype.makeGuess = function (guess) {
-    this.guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-    // disable new guesses unless 'playing'
-    if (this.status !== 'playing') {
-        return
-    }
+// Hangman.prototype.makeGuess = function (guess) {
+//     this.guess = guess.toLowerCase()
+//     const isUnique = !this.guessedLetters.includes(guess)
+//     const isBadGuess = !this.word.includes(guess)
+//     // disable new guesses unless 'playing'
+//     if (this.status !== 'playing') {
+//         return
+//     }
 
-    // should add unique guesses to list of guesses
-    if (isUnique) {
-        this.guessedLetters.push(guess)
-    }
-    // should decrement the guesses left if a unique guess isn't a match
-    if (isUnique && isBadGuess) {
-        this.remainingGuesses--
-    }
+//     // should add unique guesses to list of guesses
+//     if (isUnique) {
+//         this.guessedLetters.push(guess)
+//     }
+//     // should decrement the guesses left if a unique guess isn't a match
+//     if (isUnique && isBadGuess) {
+//         this.remainingGuesses--
+//     }
 
-    this.calculateStatus()
-}
+//     this.calculateStatus()
+// }
--- oop/person.js
+++ oop/person.js
@@ -1,25 +1,27 @@
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+    
+        return bio
+    }
 
-    return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
 const me = new Person ('Istvan', 'Acs', 29, ['coding', 'gaming'])
