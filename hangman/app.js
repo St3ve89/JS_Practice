866f442c730e9fdbf38f308c72605bc3baf95e23
Istvan Acs <istvan.acs89@gmail.com>
2018-09-16 16:58:04 +0100

    HTTP requests from javascript

--- hangman/app.js
+++ hangman/app.js
@@ -14,3 +14,16 @@ window.addEventListener('keypress', (e) => {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// Making an http request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
