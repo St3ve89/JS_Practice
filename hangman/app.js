eacb9fcd09f8df296c1238c5da606830e4d537b9
Istvan Acs <istvan.acs89@gmail.com>
2018-09-16 17:41:18 +0100

    Exploring another API

--- hangman/app.js
+++ hangman/app.js
@@ -19,11 +19,38 @@ window.addEventListener('keypress', (e) => {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+
+
+
+const countryCode = 'MX'
+
+// make new request for all countries
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', e => {
+    // parse the responseText to get back the array of objects
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        // find your country object by it's country code (alpha2Code property)
+        const country = data.find(country => country.alpha2Code === countryCode)
+        // print the full country name (name property)
+        console.log(country.name)
+    } else if (e.target.readyState === 4) {
+        console.log('Unable to fetch data')
+    }
+
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
