3fc9a4ba985ee13f2d1127e77e42c2430063c675
Istvan Acs <istvan.acs89@gmail.com>
2018-09-22 21:02:33 +0100

    Setting up the hangman app theme

--- hangman/scripts/app.js
+++ hangman/scripts/app.js
@@ -15,8 +15,14 @@ window.addEventListener('keypress', (e) => {
 })
 
 const render = () => {
-    puzzleEl.textContent = game1.puzzle
+    puzzleEl.innerHTML = ''
     guessesEl.textContent = game1.statusMessage
+
+    game1.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        puzzleEl.appendChild(letterEl)
+    })
 }
 
 const startGame = async () => {
@@ -25,7 +31,8 @@ const startGame = async () => {
     render()
 }
 
-document.querySelector('.reset').addEventListener('click', startGame)
+
+document.querySelector('#reset').addEventListener('click', startGame)
 
 startGame()
 
