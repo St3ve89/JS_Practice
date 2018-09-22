6971346076c92bb46e0b9bef76771330c1c00c08
Istvan Acs <istvan.acs89@gmail.com>
2018-09-22 18:48:36 +0100

    Async-await functions

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,15 +1,27 @@
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
     // Making an http request
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if(response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then((data) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+
+    if (response.status === 200) {
+        const data = await response.json()
         return data.puzzle
-    })
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
 }
+
+// const getPuzzle = (wordCount) => {
+//     // Making an http request
+//     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+//         if(response.status === 200) {
+//             return response.json()
+//         } else {
+//             throw new Error('Unable to fetch puzzle')
+//         }
+//     }).then((data) => {
+//         return data.puzzle
+//     })
+// }
 
 const getCountry = (countryCode) => {
     return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
