856fdc7a9fedef72723280abff5da18c6f7f5e56
Istvan Acs <istvan.acs89@gmail.com>
2018-10-27 14:36:36 +0100

    Add filters to the project

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,6 +1,21 @@
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
 
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+
+// console.log(getNotes())
+// // createNote()
+// removeNote('68c08261-3658-4ce0-9384-63372244525f')
+
+// updateNote('2c931800-8f66-41e0-a6e4-f6d43eea9445', {
+//     title: 'My note title',
+//     body: 'This is the body for my note.'
+// })
+// console.log(getNotes())
+
+console.log(getFilters())
+setFilters({
+    searchText: 'Office',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
--- notes-app/src/notes.js
+++ notes-app/src/notes.js
@@ -38,6 +38,73 @@ const createNote = () => {
     saveNotes()
 }
 
+// remove a note from the list
+const removeNote = (id) => {
+    const noteIndex = notes.findIndex((note) => note.id === id)
+
+    if(noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+        saveNotes()
+    }
+}
+
+//  sort notes by one of three ways
+const sortNotes = (sortBy) => {
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b. createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
+const updateNote = (id, updates) => {
+    const note = notes.find((note) => note.id === id)
+
+    if(!note) {
+        return
+    }
+
+    if(typeof updates.title === 'string') {
+        note.title = updates.title
+        note.updatedAt = moment().valueOf()
+    }
+
+    if(typeof updates.body === 'string') {
+        note.body = updates.body
+        note.updatedAt = moment().valueOf()
+    }
+
+    saveNotes()
+}
+
 notes = loadNotes()
 
-export { getNotes, createNote }
+export { getNotes, createNote, removeNote, sortNotes, updateNote }
