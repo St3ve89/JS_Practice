42c40a55effeffdbf94f4b0116cfa31b7cd2ca1f
Istvan Acs <istvan.acs89@gmail.com>
2018-10-29 11:05:37 +0000

    Add functionality to filters and todos

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -14,3 +14,10 @@
 // Set up form submission handler
 
 // Bonus: Add a watcher for local storage
+
+
+import { getTodos, createTodo,removeTodo, toggleTodo } from './todos'
+
+console.log(getTodos())
+removeTodo('c60cdae1-5dbc-4ead-90df-1101e5bb10a4')
+console.log(getTodos())
--- todo-app/src/todos.js
+++ todo-app/src/todos.js
@@ -1,27 +1,77 @@
+import uuidv4 from 'uuid/v4'
+
 // Setup the empty todos array
+let todos = []
 
 // loadTodos
 // Arguments: none
 // Return value: none
+// fetch existing todos from local storage
+const loadTodos = () => {
+    const todosJSON = localStorage.getItem('todos')
+
+    // handling application error
+    try {
+        todos = todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        todos = []
+    }
+}
 
 // saveTodos
 // Arguments: none
 // Return value: none
+// save todos to local storage
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
 
 // getTodos
 // Arguments: none
 // Return value: todos array
+const getTodos = () => todos
 
 // createTodo
 // Arguments: todo text
 // Return value: none
+const createTodo = (text) => {
+    todos.push({
+        id: uuidv4(),
+        text: text,
+        completed: false
+    })
+    saveTodos()
+}
 
 // removeTodo
 // Arguments: id of todo to remove
 // Return value: none
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.id === id
+    })
+
+    if(todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+        saveTodos()
+    }
+}
 
 // toggleTodo
 // Arguments: id of todo to toggle
 // Return value: none
+// toggle the completed value for a given todo
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
+
+    if(todo) {
+        todo.completed = !todo.completed
+        saveTodos()
+    }
+}
 
 // Make sure to call loadTodos and setup the exports
+loadTodos()
+
+export { getTodos, createTodo,removeTodo, toggleTodo }
