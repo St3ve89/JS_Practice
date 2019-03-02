5ee69935dec21fbd1615876a5e443e93352c5325
Istvan Acs <istvan.acs89@gmail.com>
2019-03-02 20:34:18 +0000

    Using the console

--- modern-js/js_sandbox/app.js
+++ modern-js/js_sandbox/app.js
@@ -1,2 +1,29 @@
-const h1 = document.querySelector('h1');
-console.log(h1);
+// Log to console
+console.log('Hello Szerelmeeeem!!!! <3');
+console.log(123);
+console.log(true);
+var greeting = 'hello';
+console.log(greeting);
+console.log([1,2,3,4,5,'en','nagyon','szerelmes','vagyok','beled']);
+console.log({
+  number: 1,
+  string: 'Hello',
+  boolean: true
+});
+console.table({string:'asd'});
+console.table({
+  number: 1,
+  string: 'Hello',
+  boolean: true
+});
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning!!!');
+console.time('Hello');
+  console.log('Hello Szerelmeeeem!!!! <3');
+  console.log('Hello Szerelmeeeem!!!! <3');
+  console.log('Hello Szerelmeeeem!!!! <3');
+  console.log('Hello Szerelmeeeem!!!! <3');
+  console.log('Hello Szerelmeeeem!!!! <3');
+  console.log('Hello Szerelmeeeem!!!! <3');
+console.timeEnd('Hello');
