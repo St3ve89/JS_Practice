91346cc05362359a5596bcfc0beeb4b7523cfc0a
Istvan Acs <istvan.acs89@gmail.com>
2019-03-16 20:36:29 +0000

    Getters and Setters

--- objects/index.js
+++ objects/index.js
@@ -5,21 +5,28 @@ function Circle(radius) {
 
   let defaultLocation = { x: 0, y: 0 };
 
-  let computeOptimumLocation = function(factor) {
-    // ...
+  this.getDefaultLocation = function() {
+    return defaultLocation;
   }
-
-
   this.draw = function() {
-    computeOptimumLocation(0.1);
-    // defaultLocation
-
-    // this.location
     console.log('draw');
   }
+
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function() {
+      return defaultLocation
+    },
+    set: function(value) {
+      if (!value.x || !value.y) {
+        throw new Error('invalid location')
+      }
+
+      defaultLocation = value;
+    }
+  })
 }
 
 
 const circle = new Circle(10);
-circle.
-Circle.draw();
+circle.defaultLocation = 1;
+// Circle.draw();
