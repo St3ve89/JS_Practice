db6640b865be4d67b4b69b991378400686a66e37
Istvan Acs <istvan.acs89@gmail.com>
2019-03-16 20:10:01 +0000

    Abstraction

--- objects/index.js
+++ objects/index.js
@@ -2,22 +2,21 @@
 
 function Circle(radius) {
   this.radius = radius;
+
+  this.defaultLocation = { x: 0, y: 0 };
+
+
+  this.computeOptimumLocation = function() {
+    // ...
+  }
+
+
   this.draw = function() {
+    this.computeOptimumLocation();
     console.log('draw');
   }
 }
 
 
 const circle = new Circle(10);
-
-for (let key in circle) {
-  if(typeof circle[key] !== 'function')
-  console.log(key, circle[key])
-}
-
-const keys = Object.keys(circle)
-console.log(keys)
-
-if ('radius' in circle) {
-  console.log('circle has a radius')
-}
+circle.computeOptimumLocation();
