6a0839f6d6064f96dc266933d3cee585f92117b6
Istvan Acs <istvan.acs89@gmail.com>
2019-03-18 07:38:21 +0000

    Object exercise

--- objects/exercise.js
+++ objects/exercise.js
@@ -1,40 +1,48 @@
 function StopWatch() {
   let startTime, endTime, running, duration = 0;
 
-
-  this.reset = function() {
-    startTime = null;
-    endTime = null;
-    running = false;
-    duration = 0;
-  }
-
-  this.start = function() {
-    if (running) {
-      throw new Error('Stopwatch has already started.');
-    }
-
-    running = true;
-
-    startTime = new Date();
-  }
-
-  this.stop = function() {
-    if (!running) {
-      throw new Error('Stopwatch is not started.');
-    };
-
-    running = false;
-
-    endTime = new Date();
-
-    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
-  }
-
   Object.defineProperty(this, 'duration', {
     get: function() { return duration; }
   })
+  Object.defineProperty(this, 'startTime', {
+    get: function() { return startTime; }
+  })
+  Object.defineProperty(this, 'endTime', {
+    get: function() { return endTime; }
+  })
+  Object.defineProperty(this, 'running', {
+    get: function() { return running; }
+  })
 }
 
+
+StopWatch.prototype.reset = function() {
+  this.startTime = null;
+  this.endTime = null;
+  this.running = false;
+  duration = 0;
+}
+
+StopWatch.prototype.start = function() {
+  if (this.running) {
+    throw new Error('Stopwatch has already started.');
+  }
+
+  this.running = true;
+
+  this.startTime = new Date();
+}
+
+StopWatch.prototype.stop = function() {
+  if (!this.running) {
+    throw new Error('Stopwatch is not started.');
+  };
+
+  this.running = false;
+
+  this.endTime = new Date();
+
+  const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+  duration += seconds;
+}
 const sw = new StopWatch();
