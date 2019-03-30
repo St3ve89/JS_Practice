fb78f8324fa72df87adf5a82adc2abd5aa912a7d
Istvan Acs <istvan.acs89@gmail.com>
2019-03-30 15:35:50 +0000

    ES6 Classes

--- objects/index.js
+++ objects/index.js
@@ -1,23 +1,38 @@
-function Circle(radius) {
-  // instance members
-  this.radius = radius
+// function Circle(radius) {
+//   // instance members
+//   this.radius = radius
 
-  this.move = function () {
-    this.draw();
-    console.log('move');
+//   this.move = function () {
+//     this.draw();
+//     console.log('move');
+//   }
+// }
+
+
+// // Prototype members
+// Circle.prototype.draw = function() {
+//   console.log('draw');
+// }
+
+// const c1 = new Circle(1);
+
+// // Returns instance memebers
+// console.log(Object.keys(c1));
+
+// // Returns all members(instance + prototype)
+// for ( let key in c1) console.log(key);
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+    this.move = function() {
+      
+    };
+  }
+
+  draw() {
+    console.log('draw');
   }
 }
 
-
-// Prototype members
-Circle.prototype.draw = function() {
-  console.log('draw');
-}
-
-const c1 = new Circle(1);
-
-// Returns instance memebers
-console.log(Object.keys(c1));
-
-// Returns all members(instance + prototype)
-for ( let key in c1) console.log(key);
+const c = new Circle(1)
