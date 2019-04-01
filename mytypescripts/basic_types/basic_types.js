1f39fecbba060f9d974add8bf49e023a8708c303
Istvan Acs <istvan.acs89@gmail.com>
2019-04-01 21:28:40 +0100

    Basic types with typescript

---
+++ mytypescripts/basic_types/basic_types.js
@@ -0,0 +1,11 @@
+// Numbers
+// let x: number;
+// x = 5;
+// x = 2.3;
+// x = 5 + 5;
+// x = Math.random();
+// x = 0xf00d
+// Strings
+var x;
+x = 'hello world';
+console.log(x);
---
+++ mytypescripts/basic_types/basic_types.ts
@@ -0,0 +1,18 @@
+// Numbers
+
+// let x: number;
+
+
+// x = 5;
+// x = 2.3;
+// x = 5 + 5;
+// x = Math.random();
+// x = 0xf00d
+
+
+
+// Strings
+let x: string;
+
+x = 'hello world'
+console.log(x);
