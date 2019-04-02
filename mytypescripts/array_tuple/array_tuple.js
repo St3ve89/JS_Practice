e220dd175e649404c64d193555f54d1acfd60302
Istvan Acs <istvan.acs89@gmail.com>
2019-04-02 20:43:03 +0100

    Array and tuple

---
+++ mytypescripts/array_tuple/array_tuple.js
@@ -0,0 +1,15 @@
+// let x: number[];
+// let y: string[];
+// let z: boolean[]
+// // x = [1,2,3]
+// // x=["hello", "1"]
+// // y=[1,2,3]
+// // y=['hello','world','!']
+// z=[true,false]
+// console.log(z)
+// let x: Array<number>;
+// x = [1,2,1]
+// console.log(x)
+var x;
+x = [1, 'hello'];
+console.log(x);
---
+++ mytypescripts/array_tuple/array_tuple.ts
@@ -0,0 +1,24 @@
+// let x: number[];
+// let y: string[];
+// let z: boolean[]
+
+// // x = [1,2,3]
+
+// // x=["hello", "1"]
+// // y=[1,2,3]
+// // y=['hello','world','!']
+// z=[true,false]
+// console.log(z)
+
+// let x: Array<number>;
+
+// x = [1,2,1]
+// console.log(x)
+
+
+let x: [number, string]
+
+
+x = [1, 'hello']
+
+console.log(x);
