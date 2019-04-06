283af392ec307526fd3570fa422c1aa47f2092e1
Istvan Acs <istvan.acs89@gmail.com>
2019-04-06 10:51:52 +0100

    Let and const block scoping

---
+++ mytypescripts/let_const/let_const.js
@@ -0,0 +1,24 @@
+// let a = 55;
+// let a = 45
+// console.log(a)
+// function showVar() {
+//   var a = 33;
+//   if(true) {
+//     var a = 44
+//     console.log(a)
+//   }
+//   console.log(a)
+// }
+function showLet() {
+    var a = 33;
+    if (true) {
+        var a_1 = 44;
+        console.log(a_1);
+    }
+    console.log(a);
+}
+showLet();
+var colors = [];
+colors.push('red');
+colors.push('blue');
+console.log(colors);
---
+++ mytypescripts/let_const/let_const.ts
@@ -0,0 +1,34 @@
+// let a = 55;
+// let a = 45
+
+// console.log(a)
+
+// function showVar() {
+//   var a = 33;
+//   if(true) {
+//     var a = 44
+//     console.log(a)
+//   }
+//   console.log(a)
+// }
+
+
+function showLet() {
+  let a = 33;
+  if(true) {
+    let a = 44
+    console.log(a)
+  }
+  console.log(a)
+}
+
+
+showLet()
+
+
+const colors = []
+
+colors.push('red')
+colors.push('blue')
+
+console.log(colors)
