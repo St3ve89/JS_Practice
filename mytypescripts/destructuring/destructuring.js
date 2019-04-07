b248a1f5e9ddc4cbace3692dcf83d93a3d84b7ad
Istvan Acs <istvan.acs89@gmail.com>
2019-04-07 15:44:44 +0100

    Destructuring

---
+++ mytypescripts/destructuring/destructuring.js
@@ -0,0 +1,28 @@
+// const user = {
+//   first: 'Jhon',
+//   last: 'Doe'
+// }
+// const {first, last} = user
+// console.log(first)
+// const users = ['mark', 'jeff', 'carlos']
+// const [user1, user2, user3] = users
+// console.log(user2)
+// const [a,b] = ['x', 'y']
+// console.log(a)
+// const obj = {
+//   a:[
+//     {
+//       foo: 123,
+//       bar: 'abc'
+//     }, 
+//     {
+//     }
+//   ],
+//   b: true
+// }
+// const {a: [{bar: b}]} = obj
+// console.log(b)
+// const {x: x} = {x: 12, y: 22};
+// console.log(x)
+// const [x] = []
+// console.log(x)
