531f0ddc01aff48129728b6eb23773218beb16d3
Istvan Acs <istvan.acs89@gmail.com>
2019-11-30 11:31:33 +0000

    Adding, modifying and deleting properties on objects

---
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,12 @@
+let person = {
+  name: 'Istvan',
+  age: 30,
+  hobbies: ['sports', 'Cooking'],
+  greet: function() {
+    alert('Hi there!');
+  }
+};
+// delete person.age;
+person.isAdmin = true;
+
+console.log(person);
