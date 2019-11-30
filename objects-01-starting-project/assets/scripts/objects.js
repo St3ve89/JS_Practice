876c0e9c5205b2645e09adf9323b6c4985fefedc
Istvan Acs <istvan.acs89@gmail.com>
2019-11-30 12:09:22 +0000

    Shorthand property syntax

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,17 +1,30 @@
-const movieList = document.getElementById('movie-list');
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('searchBtn');
 
-movieList.style['background-color'] = 'red';
-movieList.style['display'] = 'block';
+const movies = [];
 
-let person = {
-  name: 'Istvan',
-  age: 30,
-  hobbies: ['sports', 'Cooking'],
-  greet: function() {
-    alert('Hi there!');
-  }
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  )
+    return;
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
 };
-// delete person.age;
-person.isAdmin = true;
 
-console.log(person);
+addMovieBtn.addEventListener('click', addMovieHandler);
